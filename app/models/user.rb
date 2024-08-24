b7a2dfd5d26d53c19d27906a5c1167f73f04f3f4
Pup Gray <pupgray@outlook.com>
2024-08-24 16:47:49 -0400

    Oh this too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, uniqueness: true, length: { in: 4..32 }
+  validates :username, uniqueness: true, length: { in: 4..32 }, format: { with: /\A[\w]+\z/,
+                                                                          message: "only allows letters, numbers, and underscore" }
+
+  normalizes :username, with: ->(username) { username.downcase.strip }
 end
